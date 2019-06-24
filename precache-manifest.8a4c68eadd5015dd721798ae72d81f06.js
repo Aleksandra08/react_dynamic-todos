5b91a64e8f4bcf075f8a588b3ec8d1fc1bccc24f
Aleksandra <starodub.alexandra@gmail.com>
2019-06-24 16:12:31 +0300

    Updates

---
+++ precache-manifest.8a4c68eadd5015dd721798ae72d81f06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b060a2895928a8750b1257739eb4ed23",
+    "url": "/react_dynamic-todos/index.html"
+  },
+  {
+    "revision": "a4e4c295901d8d99ddb1",
+    "url": "/react_dynamic-todos/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react_dynamic-todos/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "a4e4c295901d8d99ddb1",
+    "url": "/react_dynamic-todos/static/js/main.f5b0669b.chunk.js"
+  },
+  {
+    "revision": "167b4a055810a3cd9138",
+    "url": "/react_dynamic-todos/static/js/runtime~main.410e91bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_dynamic-todos/static/media/logo.5d5d9eef.svg"
+  }
+]);
